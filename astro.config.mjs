3576fa9367fb75c597121c8a6adbbc82edfa197f
ASIER ESCOFET <aescofet@stockagile.com>
2025-04-05 20:32:30 +0200

    check

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://workout-tracker-app.github.io',
+  base: '/landing',
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "en",
