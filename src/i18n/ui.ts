9649c7dc7221208f70ec8e5b2a69bff991592f3d
Bernat Osorio <bosorigo@gmail.com>
2025-04-06 16:56:44 +0200

    add translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,16 +11,26 @@ export const ui = {
     en: {
         'nav.quickLinks': 'Quick links',
         'nav.privacyPolicy': 'Privacy policy',
-        'nav.deleteAccount': 'Delete account',
+        'nav.deleteAccount': 'How to delete your account',
         'nav.spanish': 'Spanish',
         'nav.english': 'English',
+        'intro.title1': 'Save,',
+        'intro.title2': 'share and analyze your workouts',
+        'intro.subtitle': 'Track your workouts, share your progress, and stay motivated with your fitness community.',
+        'footer.description': 'Track your workouts, share your progress, and stay motivated with your fitness community.',
+        'footer.allRightsReserved': '2025 Postfit. All rights reserved.',
     },
     es: {
         'nav.quickLinks': 'Enlaces rápidos',
         'nav.privacyPolicy': 'Politica de privacidad',
-        'nav.deleteAccount': 'Borrar cuenta',
+        'nav.deleteAccount': 'Como borrar mi cuenta',
         'nav.spanish': 'Español',
         'nav.english': 'Inglés',
+        'intro.title1': 'Guardar,',
+        'intro.title2': 'comparte y analiza tus entrenamientos',
+        'intro.subtitle': 'Realiza un seguimiento de tus entrenamientos, comparte tu progreso y mantente motivado con tu comunidad de fitness.',
+        'footer.description': 'Realiza un seguimiento de tus entrenamientos, comparte tu progreso y mantente motivado con tu comunidad de fitness.',
+        'footer.allRightsReserved': '2025 Postfit. Todos los derechos están reservados.',
 
     },
 } as const;
